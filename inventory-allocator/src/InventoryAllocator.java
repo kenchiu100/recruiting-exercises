9fb6e798232ab441f46cf8c6b9d0e20008b2187a
Ka Kin Chiu <aeaopa79191@gmail.com>
2019-05-03 00:51:49 -0700

    fixed bugs in InventoryAllocator; finsihed JUnit tests

--- inventory-allocator/src/InventoryAllocator.java
+++ inventory-allocator/src/InventoryAllocator.java
@@ -1,46 +1,68 @@
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class InventoryAllocator {
-	// assume the list of warehouses is pre-sorted based on cost.
-	//The first warehouse will be less expensive to ship from than the second warehouse.
-	//This function will ship partially if there is not enough inventory
-	public static List<Map<String, Map<String, Integer>>> findCheapestShipment(
+/*	Assume the list of warehouses is pre-sorted based on cost.
+	The first warehouse will be less expensive to ship from than the second warehouse.
+	This function will ship partially if there is not enough inventory 
+*/
+	public static Map<String, Map<String, Integer>> findCheapestShipment(
 			Map<String, Integer> order,
 			List<InventoryDistribution> warehouses){
-		List<Map<String, Map<String, Integer>>> shipments = new ArrayList<>();
-		
+		Map<String, Map<String, Integer>> warehouseOrders = new HashMap<>();
+		//	warehouses-> items -> amount
+		// get the items that can be shipped without splitting
 		for(InventoryDistribution warehouse : warehouses){
 			Map<String, Integer> inventory = warehouse.getInventory();
 			String name = warehouse.getName();
 			if(order.size() == 0) break;
-			Map<String, Map<String, Integer>> warehouseOrder = new HashMap<>();
-			
 			for(String item : order.keySet()){
 				int neededAmount = order.get(item);
+				if(neededAmount<=0) continue;
+				if(inventory.containsKey(item)){
+					int invenAmount = inventory.get(item);		
+					if(invenAmount>=neededAmount){
+						addOrderToWarehouseOrders(warehouseOrders, name, item, neededAmount);
+						order.put(item, 0); // update the order
+					}
+				}
+			}
+		}
+		
+		//get items that needs to be split
+		for(InventoryDistribution warehouse : warehouses){
+			Map<String, Integer> inventory = warehouse.getInventory();
+			String name = warehouse.getName();
+			if(order.size() == 0) break;
+			for(String item : order.keySet()){
+				int neededAmount = order.get(item);
+				if(neededAmount<=0) continue;
 				if(inventory.containsKey(item)){
 					int invenAmount = inventory.get(item);
 					if(invenAmount>=neededAmount){
-						Map<String, Integer> orderAmount = new HashMap<>();
-						orderAmount.put(item, neededAmount);
-						warehouseOrder.put(name, orderAmount);
-						order.remove(item); // update the order
-					}else if(invenAmount >0){ // ship partially
-						Map<String, Integer> orderAmount = new HashMap<>();
-						orderAmount.put(item, invenAmount);	
-						warehouseOrder.put(name, orderAmount);
+						addOrderToWarehouseOrders(warehouseOrders, name, item, neededAmount);
+						order.put(item, 0); // update the order
+					}
+					else if(invenAmount >0){ // ship partially						
+						addOrderToWarehouseOrders(warehouseOrders, name, item, invenAmount);					
 						order.put(item, neededAmount - invenAmount); // update the order
 					}
 				}
 			}
-			if(warehouseOrder.size()>0){
-				shipments.add(warehouseOrder);
-			}
 		}
-		
-		return shipments;
-		
+		return warehouseOrders;		
+	}
+	
+	private static void addOrderToWarehouseOrders(Map<String, Map<String, Integer>> warehouseOrders, 
+			String warehouse, String item, int amount){
+		if(warehouseOrders.containsKey(warehouse)){
+			Map<String, Integer> inventoryMap = warehouseOrders.get(warehouse);
+			inventoryMap.put(item, amount);
+		}else{
+			Map<String, Integer> orderAmount = new HashMap<>();
+			orderAmount.put(item, amount);
+			warehouseOrders.put(warehouse, orderAmount);							
+		}
 	}
 }
--- inventory-allocator/src/InventoryAllocatorTest.java
+++ inventory-allocator/src/InventoryAllocatorTest.java
@@ -1,12 +1,262 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.junit.Test;
 
 public class InventoryAllocatorTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testfindCheapestShipment1() {
+		//setting up inputs
+		Map<String, Integer> inventory1 = new HashMap<>();
+		inventory1.put("apple", 1);
+		InventoryDistribution warehouse1 = new InventoryDistribution("owd", inventory1);
+		List<InventoryDistribution> warehouses = new ArrayList<>();
+		warehouses.add(warehouse1);
+		Map<String, Integer> order = new HashMap<>();
+		order.put("apple",1);
+		
+		Map<String, Map<String, Integer>> shipments = InventoryAllocator.findCheapestShipment(order, warehouses);
+		
+		//setting up expected output
+		Map<String, Integer> orderAmount = new HashMap<>();
+		Map<String, Map<String, Integer>> expectedShipments = new HashMap<>();
+		orderAmount.put("apple", 1);
+		expectedShipments.put("owd", orderAmount);
+		assertEquals(expectedShipments, shipments);
+		
 	}
+	
+	@Test
+	public void testfindCheapestShipment2() {
+		//setting up inputs
+		Map<String, Integer> inventory1 = new HashMap<>();
+		inventory1.put("apple", 0);
+		InventoryDistribution warehouse1 = new InventoryDistribution("owd", inventory1);
+		List<InventoryDistribution> warehouses = new ArrayList<>();
+		warehouses.add(warehouse1);
+		Map<String, Integer> order = new HashMap<>();
+		order.put("apple",1);
+		Map<String, Map<String, Integer>> shipments = InventoryAllocator.findCheapestShipment(order, warehouses);
+		
+		//setting up expected output
+		
+		Map<String, Map<String, Integer>> expectedShipments = new HashMap<>();
+		assertEquals(expectedShipments, shipments);
+	}
+	
+	@Test
+	public void testfindCheapestShipment3() {
+		//setting up inputs
+		List<InventoryDistribution> warehouses = new ArrayList<>();
+		Map<String, Integer> inventory1 = new HashMap<>();
+		inventory1.put("apple", 5);
+		InventoryDistribution warehouse1 = new InventoryDistribution("owd", inventory1);
+		warehouses.add(warehouse1);
+		Map<String, Integer> inventory2 = new HashMap<>();
+		inventory2.put("apple", 5);
+		InventoryDistribution warehouse2 = new InventoryDistribution("dm", inventory2);
+		warehouses.add(warehouse2);		
+		
+		Map<String, Integer> order = new HashMap<>();
+		order.put("apple",10);
+		
+		Map<String, Map<String, Integer>> shipments = InventoryAllocator.findCheapestShipment(order, warehouses);
+		
+		//setting up expected output
+		Map<String, Map<String, Integer>> expectedShipments = new HashMap<>();
+		Map<String, Integer> orderAmount1 = new HashMap<>();
+		orderAmount1.put("apple", 5);
+		expectedShipments.put("owd", orderAmount1);
+		Map<String, Integer> orderAmount2 = new HashMap<>();
+		orderAmount2.put("apple", 5);
+		expectedShipments.put("dm", orderAmount2);
+		assertEquals(expectedShipments, shipments);
+	}
+	
+	@Test
+	//prefer not splitting an item than getting items from cheaper warehouses
+	public void testfindCheapestShipment4() {
+		//setting up inputs
+		List<InventoryDistribution> warehouses = new ArrayList<>();
+		Map<String, Integer> inventory1 = new HashMap<>();
+		inventory1.put("apple", 4);
+		InventoryDistribution warehouse1 = new InventoryDistribution("owd", inventory1);
+		warehouses.add(warehouse1);
+		Map<String, Integer> inventory2 = new HashMap<>();
+		inventory2.put("apple", 4);
+		InventoryDistribution warehouse2 = new InventoryDistribution("dm", inventory2);
+		warehouses.add(warehouse2);		
+		Map<String, Integer> inventory3 = new HashMap<>();
+		inventory3.put("apple", 10);
+		InventoryDistribution warehouse3 = new InventoryDistribution("ga", inventory3);
+		warehouses.add(warehouse3);	
+		Map<String, Integer> order = new HashMap<>();
+		order.put("apple",10);
+		
+		Map<String, Map<String, Integer>> shipments = InventoryAllocator.findCheapestShipment(order, warehouses);
+		
+		//setting up expected output
+		Map<String, Map<String, Integer>> expectedShipments = new HashMap<>();
+		Map<String, Integer> orderAmount1 = new HashMap<>();
+		orderAmount1.put("apple", 10);
+		expectedShipments.put("ga", orderAmount1);
 
+		assertEquals(expectedShipments, shipments);
+	}
+	
+	@Test
+	//prefer cheaper shipment when it doesn't need to split
+	public void testfindCheapestShipment5() {
+		//setting up inputs
+		List<InventoryDistribution> warehouses = new ArrayList<>();
+		Map<String, Integer> inventory1 = new HashMap<>();
+		inventory1.put("apple", 4);
+		InventoryDistribution warehouse1 = new InventoryDistribution("owd", inventory1);
+		warehouses.add(warehouse1);
+		Map<String, Integer> inventory2 = new HashMap<>();
+		inventory2.put("apple", 10);
+		InventoryDistribution warehouse2 = new InventoryDistribution("dm", inventory2);
+		warehouses.add(warehouse2);		
+		Map<String, Integer> inventory3 = new HashMap<>();
+		inventory3.put("apple", 10);
+		InventoryDistribution warehouse3 = new InventoryDistribution("ga", inventory3);
+		warehouses.add(warehouse3);	
+		Map<String, Integer> order = new HashMap<>();
+		order.put("apple",10);
+		
+		Map<String, Map<String, Integer>> shipments = InventoryAllocator.findCheapestShipment(order, warehouses);
+		
+		//setting up expected output
+		Map<String, Map<String, Integer>> expectedShipments = new HashMap<>();
+		Map<String, Integer> orderAmount1 = new HashMap<>();
+		orderAmount1.put("apple", 10);
+		expectedShipments.put("dm", orderAmount1);
+
+		assertEquals(expectedShipments, shipments);
+	}
+	
+	@Test
+	//not getting more item than the order required
+	public void testfindCheapestShipment6() {
+		//setting up inputs
+		List<InventoryDistribution> warehouses = new ArrayList<>();
+		Map<String, Integer> inventory1 = new HashMap<>();
+		inventory1.put("apple", 4);
+		InventoryDistribution warehouse1 = new InventoryDistribution("owd", inventory1);
+		warehouses.add(warehouse1);
+		Map<String, Integer> inventory3 = new HashMap<>();
+		inventory3.put("apple", 14);
+		InventoryDistribution warehouse3 = new InventoryDistribution("ga", inventory3);
+		warehouses.add(warehouse3);	
+		Map<String, Integer> order = new HashMap<>();
+		order.put("apple",10);
+		
+		Map<String, Map<String, Integer>> shipments = InventoryAllocator.findCheapestShipment(order, warehouses);
+		
+		//setting up expected output
+		Map<String, Map<String, Integer>> expectedShipments = new HashMap<>();
+		Map<String, Integer> orderAmount1 = new HashMap<>();
+		orderAmount1.put("apple", 10);
+		expectedShipments.put("ga", orderAmount1);
+		assertEquals(expectedShipments, shipments);
+	}
+	
+	@Test
+	//ship partially if not enough inventory across all warehouses
+	public void testfindCheapestShipment7() {
+		//setting up inputs
+		List<InventoryDistribution> warehouses = new ArrayList<>();
+		Map<String, Integer> inventory1 = new HashMap<>();
+		inventory1.put("apple", 4);
+		InventoryDistribution warehouse1 = new InventoryDistribution("owd", inventory1);
+		warehouses.add(warehouse1);
+		Map<String, Integer> inventory2 = new HashMap<>();
+		inventory2.put("apple", 4);
+		InventoryDistribution warehouse2 = new InventoryDistribution("dm", inventory2);
+		warehouses.add(warehouse2);		
+		Map<String, Integer> inventory3 = new HashMap<>();
+		inventory3.put("apple", 10);
+		InventoryDistribution warehouse3 = new InventoryDistribution("ga", inventory3);
+		warehouses.add(warehouse3);	
+		Map<String, Integer> order = new HashMap<>();
+		order.put("apple",99);
+		
+		Map<String, Map<String, Integer>> shipments = InventoryAllocator.findCheapestShipment(order, warehouses);
+		
+		//setting up expected output
+		Map<String, Map<String, Integer>> expectedShipments = new HashMap<>();
+		Map<String, Integer> orderAmount1 = new HashMap<>();
+		orderAmount1.put("apple", 4);
+		expectedShipments.put("owd", orderAmount1);
+		Map<String, Integer> orderAmount2 = new HashMap<>();
+		orderAmount2.put("apple", 4);
+		expectedShipments.put("dm", orderAmount2);
+		Map<String, Integer> orderAmount3 = new HashMap<>();
+		orderAmount3.put("apple", 10);
+		expectedShipments.put("ga", orderAmount3);
+
+		assertEquals(expectedShipments, shipments);
+	}
+	
+	@Test
+	//some items have to split, while some shouldn't split
+	public void testfindCheapestShipment8() {
+		//setting up inputs
+		List<InventoryDistribution> warehouses = new ArrayList<>();
+		Map<String, Integer> inventory1 = new HashMap<>();
+		inventory1.put("apple", 5);
+		inventory1.put("orange", 5);
+		inventory1.put("iphone", 0);
+		inventory1.put("watermelon", 10);		
+		InventoryDistribution warehouse1 = new InventoryDistribution("owd", inventory1);
+		warehouses.add(warehouse1);
+		Map<String, Integer> inventory2 = new HashMap<>();
+		inventory2.put("apple", 5);
+		inventory2.put("orange", 99);
+		inventory2.put("iphone", 20);
+		inventory2.put("watermelon", 8);		
+		InventoryDistribution warehouse2 = new InventoryDistribution("dm", inventory2);
+		warehouses.add(warehouse2);		
+		Map<String, Integer> inventory3 = new HashMap<>();
+		inventory3.put("apple", 15);
+		inventory3.put("orange", 50);
+		inventory3.put("iphone", 3);
+		InventoryDistribution warehouse3 = new InventoryDistribution("ga", inventory3);
+		warehouses.add(warehouse3);	
+		Map<String, Integer> order = new HashMap<>();
+		order.put("apple",20);
+		order.put("orange",50);
+		order.put("banana", 3);
+		order.put("iphone", 35);
+		order.put("watermelon", 18);
+
+		
+		Map<String, Map<String, Integer>> shipments = InventoryAllocator.findCheapestShipment(order, warehouses);
+		
+		//setting up expected output
+		Map<String, Map<String, Integer>> expectedShipments = new HashMap<>();
+		Map<String, Integer> orderAmount1 = new HashMap<>();
+		orderAmount1.put("apple", 5);
+		orderAmount1.put("watermelon", 10);
+		expectedShipments.put("owd", orderAmount1);
+		Map<String, Integer> orderAmount2 = new HashMap<>();
+		orderAmount2.put("apple", 5);
+		orderAmount2.put("orange", 50);
+		orderAmount2.put("iphone", 20);
+		orderAmount2.put("watermelon", 8);
+		expectedShipments.put("dm", orderAmount2);
+		Map<String, Integer> orderAmount3 = new HashMap<>();
+		orderAmount3.put("apple", 10);
+		orderAmount3.put("iphone", 3);
+		expectedShipments.put("ga", orderAmount3);
+
+		assertEquals(expectedShipments, shipments);
+	}
+	
+	
 }
